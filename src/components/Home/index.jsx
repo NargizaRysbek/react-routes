af2e2a049adb106457b072740202665625eff414
“NargizaRysbek” <r.nakosya@gmail.com>
2021-05-27 02:08:23 +0600

    added spinner loading

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Avatar } from 'antd'
+import { Card, Avatar, Spin } from 'antd'
 
 export const Home = () => {
 
@@ -7,13 +7,28 @@ export const Home = () => {
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true) // дщ того как будет загружаться фотки выйдет спин
         fetch('https://jsonplaceholder.typicode.com/photos?_page=1&_limit=10')
         .then(response => response.json())
-        .then(json => setPhotos(json))
+        .then(json => {
+            setPhotos(json)
+            setLoading(false) // после загрузки фотокБ спин пропадет
+        })
     }, [])
+    
 
     return (
         <div style={styles}>
+
+             {/* если true выходит спин */}
+            
+            {
+                loading && <div style={{display: 'flex', margin: '50px auto'}}> 
+               
+                     <Spin/>  loading...
+                </div>
+               
+            }
             {
                 photos.map((el, id) => {
                     return <Card key={id} style={{ width: 300, margin: 10}}>
